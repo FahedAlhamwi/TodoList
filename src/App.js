e84ae1bf94fc5b9d37900f6839ff4e6b9067e76d
FahedAlhamwi <sadruddien@icloud.com>
2021-06-29 17:08:16 +0200

    some other changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import Todo from './components/Todo';
+import React, {useState} from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const addTodo = todo => {
+    if(!todo.text || /^\s*$/.test(todo.text)) {
+        return
+    }
+    const newTodos = [todo, ...todos]
+    setTodos(newTodos);
+  };
+  const updateTodo = (todoId, newValue) => {
+    if(!newValue.text || /^\s*$/.test(newValue.text)) {
+        return
+    }
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+  }
+  const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id);
+    setTodos(removeArr);
+  }
+  const completeTodo = id => {
+    let updateedTodos = todos.map(todo => {
+        if (todo.id === id){
+            todo.isComplete = !todo.isComplete
+        }
+        return todo
+    })
+    setTodos(updateedTodos);
+  }
   return (
     <div className="todo-app">
      <TodoList />
+     <TodoForm onSubmit={addTodo}/>
+     <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,44 +1,15 @@
-import React,{useState} from 'react'
-import Todo from './Todo';
-import TodoForm from './TodoForm'
-// Create a function using hook as an array for the list 
+import React from "react"
+
+
 function TodoList() {
-    const [todos, setTodos] = useState([]);
-    //First is adding method which is giving a new input
-    const addTodo = todo => {
-        if(!todo.text || /^\s*$/.test(todo.text)) {
-            return
-        }
-        const newTodos = [todo, ...todos]
-        setTodos(newTodos);
-    };
-    //Update method to allow you to update the input to a new value
-    const updateTodo = (todoId, newValue) => {
-        if(!newValue.text || /^\s*$/.test(newValue.text)) {
-            return
-        }
-        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
-    };
-    // Remove method to allow you to remove the input 
-    const removeTodo = id => {
-        const removeArr = [...todos].filter(todo => todo.id !== id);
-        setTodos(removeArr);
-    }
-    // Changing the apperance of the input when you push it to show its complete
-    const completeTodo = id => {
-        let updateedTodos = todos.map(todo => {
-            if (todo.id === id){
-                todo.isComplete = !todo.isComplete
-            }
-            return todo
-        })
-        setTodos(updateedTodos);
-    }
+    
+   
     return (
         <div>
-            <h1>What wife wants today!</h1>
-            <TodoForm onSubmit={addTodo}/>
-            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
+            <h1>What your wife wants today</h1>
+            <h2>In another words</h2>
+            <h1>What you <span className='red'>MUST</span>  do today</h1>
+            
             
         </div>
     )
