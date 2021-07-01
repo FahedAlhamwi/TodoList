fe842c537ec41e70bf67869811f6f46142d60aac
FahedAlhamwi <sadruddien@icloud.com>
2021-07-02 00:16:02 +0200

    mainContainer

---
+++ src/components/MainContainer.js
@@ -0,0 +1,45 @@
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+import TodoList from './TodoList';
+import React, {useState} from "react";
+
+function MainContainer() {
+    const [todos, setTodos] = useState([]);
+  const addTodo = todo => {
+    if(!todo.text || /^\s*$/.test(todo.text)) {
+        return
+    }
+    const newTodos = [todo, ...todos]
+    setTodos(newTodos);
+  };
+  const updateTodo = (todoId, newValue) => {
+    if(!newValue.text || /^\s*$/.test(newValue.text)) {
+        return
+    }
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+  }
+  const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id);
+    setTodos(removeArr);
+  }
+  const completeTodo = id => {
+    let updateedTodos = todos.map(todo => {
+        if (todo.id === id){
+            todo.isComplete = !todo.isComplete
+        }
+        return todo
+    })
+    setTodos(updateedTodos);
+  }
+  return (
+    <div className="todo-app">
+     <TodoList />
+     <TodoForm onSubmit={addTodo}/>
+     <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
+    </div>
+  );
+}
+
+
+
+export default MainContainer
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,12 @@
 import React from "react"
 
-
 function TodoList() {
     
-   
     return (
         <div>
             <h1>What your wife wants today</h1>
             <h2>In another words</h2>
             <h1>What you <span className='red'>MUST</span>  do today</h1>
-            
-            
         </div>
     )
 }
